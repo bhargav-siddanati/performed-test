8b7ce6f9d19dfbf0ec22c7a661f19b387242c476
bhargav-siddanati <bhargav77990@gmail.com>
2025-10-19 19:07:25 +0530

    Revert "[initial commited files]"
    This reverts commit a227b12601d484e2a9a1a0746786618d2c2f23ff.

--- src/main/java/com/bhargav/java/interview/oracle/Palindrom.java
+++
@@ -1,9 +0,0 @@
-package com.bhargav.java.interview.oracle;
-
-public class Palindrom {
-    public static void main(String[] args) {
-        String str = "madam";
-        String rev = new StringBuilder(str).reverse().toString();
-        System.out.println(str.equals(rev));
-    }
-}
